18656e3d82357d1d39d4613d3d2e41a9087f11a4
NickBwalley <nickbwalley@gmail.com>
2025-03-07 00:04:08 +0300

    file-handling in python

---
+++ mkdir.py
@@ -0,0 +1,18 @@
+import os
+
+def create_new_dir():
+    # Get the directory where the script is being run
+    root_dir = os.getcwd()  # Gets the current working directory
+    dir_name = "6-File-Handling"
+
+    # Define the new folder path
+    new_folder_path = os.path.join(root_dir, dir_name)
+
+    # Create the new folder if it doesn't exist
+    if not os.path.exists(new_folder_path):
+        os.mkdir(new_folder_path)
+        print(f"Folder '{dir_name}'' created successfully at {new_folder_path}")
+    else:
+        print(f"Folder '{dir_name}' already exists at {new_folder_path}")
+
+create_new_dir()
